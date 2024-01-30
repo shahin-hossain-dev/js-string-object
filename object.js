3820f05535363cc4b5ab8d67ab3d330194eef48f
shahin-hossain-dev <shahin.hossain.dev@gmail.com>
2024-01-30 23:38:04 +0600

    19_5 Introduction To Objects Properties And Values

---
+++ object.js
@@ -0,0 +1,34 @@
+// primitive data type
+
+const name = 'Shahin';
+const id = 20;
+const isPassed = true;
+const doAnything = null;
+let isDeveloper;
+
+
+// non primitive 
+
+// array
+const friends = ['Ahsan', 'Suajn', 'Faysal', 'Mohiuddin'];
+
+// console.log(friends)
+
+// object 
+
+// object variable declare করে {curly basis} দিয়ে wrap করতে হয়।
+// object various multiple data type store করতে পারে।
+
+const student = {
+    id: 20,
+    name: 'Shahin',
+    isPass: true,
+    isDeveloper: null,
+    subjects: ['physics', 'chemistry', 'psychology'],
+    exams: {
+        name: 'Final Exam',
+        marks: 100
+    }
+}
+
+console.log(student)
